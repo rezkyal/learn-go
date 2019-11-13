7f338801544723d8c52640aa57188428cd3e3c6f
rezkyal2@gmail.com <rezkyal2@gmail.com>
2019-11-13 21:51:32 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Welcome struct {
+	Name string
+	Time string
+}
+
+func main() {
+	welcome := Welcome{"Rezky", time.Now().Format(time.Stamp)}
+	templates := template.Must(template.ParseFiles("template/welcome-template.html"))
+
+	http.Handle("/statics/",
+		http.StripPrefix("/statics/",
+			http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Listening")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
